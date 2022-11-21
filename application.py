e597ef92029f4a1fa62d7bd74e61069f1f822b9a
TheBogman02 <92158821+TheBogman02@users.noreply.github.com>
2022-11-21 21:40:55 +0000

    :ambulance: publish log to db(need michael to fix fk dependency

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
-
+from dateutil.utils import today
 from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename
+import datetime
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory, PNOperationType
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
+from flask_mysqldb import MySQL
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super_secret_key'
@@ -22,12 +30,49 @@ app.config['MYSQL_DB'] = 'dmn_alarms'
 app.config['UPLOAD_FOLDER'] = 'static/images/'
 app.config['ALLOWED_EXTENSIONS'] = {'txt','pdf','png','jpg','jpeg','gif'}
 
+#PUBNUB
+myChannel = "dmn-channel"
+sensorList = ["finger_scanner, finger_scanner_new"]
+data = {}
+
+pnconfig = PNConfiguration()
+
+pnconfig.subscribe_key = 'sub-c-5efa4cb5-6f01-42ea-a6ac-98b3dccd764a'
+pnconfig.publish_key = 'pub-c-6df66f48-e71d-41ea-a2a7-d696bda5a561'
+pnconfig.uuid = 'exterior-pi'
+pubnub = PubNub(pnconfig)
+
+pubnub.subscribe()\
+  .channels(myChannel)\
+  .execute()
 
 
+
+#mysql app
 mysql = MySQL(app)
-
+#flask session
 Session(app)
 
+
+class SubscribeHandler(SubscribeCallback):
+    def message(self, pubnub, message):
+        print("Message payload: %s" % message.message)
+        print("Message publisher: %s" % message.publisher)
+
+        with app.app_context():
+            cur = mysql.connection.cursor()
+            cur.execute(''' SELECT * FROM employee_table where employee_id == message.message['finger_scanner']''')
+            account = cur.fetchone()
+
+            currentDay = today()
+            currentTime = datetime.now().time()
+            print(message.message['finger_scanner'])
+            cur.execute(''' INSERT INTO employee_access_table VALUES(null,%s,%s,%s)''', (currentDay,currentTime, account[0]))
+            mysql.connection.commit()
+            cur.close()
+
+
+pubnub.add_listener(SubscribeHandler())
 @app.route("/")
 def index():
     if not session.get("email"):
@@ -189,5 +234,12 @@ def viewEditEmployee(employee_id):
 
     return render_template("editEmployees.html", employee=employee)
 
+
+
+
+
+
 if __name__ == '__main__':
     app.run(port = 5000)
+
+
